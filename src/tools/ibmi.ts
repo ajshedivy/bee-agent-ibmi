19a1212d5b0cdd7b90a8b3ce27e5f61d3e84e17e
Adam Shedivy <ajshedivyaj@gmail.com>
2024-11-13 17:17:48 -0600

    add ibmi agent

---
+++ src/tools/ibmi.ts
@@ -0,0 +1,141 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { GetRunContext } from "bee-agent-framework/context";
+import { AnyToolSchemaLike, FromSchemaLike } from "bee-agent-framework/internals/helpers/schema";
+import {
+  Tool,
+  ToolInput,
+  ToolError,
+  BaseToolOptions,
+  BaseToolRunOptions,
+  JSONToolOutput,
+} from "bee-agent-framework/tools/base";
+import { Provider, getMetadata } from "bee-agent-framework/tools/database/metadata";
+import mapepire from "@ibm/mapepire-js";
+import { Cache } from "bee-agent-framework/cache/decoratorCache";
+import { DaemonServer } from "@ibm/mapepire-js/dist/src/types.js";
+import { Query } from "@ibm/mapepire-js/dist/src/query.js";
+import { z } from "zod";
+
+interface MapepireOptions extends DaemonServer {
+  schema: string;
+}
+
+interface ToolOptions extends BaseToolOptions {
+  provider: "ibmi";
+  connection: MapepireOptions;
+}
+type ToolRunOptions = BaseToolRunOptions;
+
+export class IBMiTool extends Tool<JSONToolOutput<any>, ToolOptions, ToolRunOptions> {
+  name = "IBMiTool";
+  description = "Converts natural language to SQL query and executes it on Db2 for i";
+
+  public constructor(options: ToolOptions) {
+    super(options);
+  }
+
+  static {
+    this.register();
+  }
+
+  @Cache()
+  protected async connection(): Promise<mapepire.SQLJob> {
+    const job = new mapepire.SQLJob();
+    try {
+      await job.connect(this.options.connection);
+      return job;
+    } catch (e) {
+      throw new ToolError(`Unable to connect to Db2 for i: ${e}`, [], {
+        isRetryable: false,
+        isFatal: true,
+      });
+    }
+  }
+
+  inputSchema() {
+    return z.object({
+      query: z.string({ description: "The SQL query to be executed." }).min(1),
+    });
+  }
+
+  protected async _run(
+    { query }: ToolInput<this>,
+    _options?: ToolRunOptions,
+  ): Promise<JSONToolOutput<any>> {
+    if (!this.isReadOnlyQuery(query)) {
+      return new JSONToolOutput({
+        success: false,
+        error: "Invalid query. Only SELECT queries are allowed.",
+      });
+    }
+    let metaData = "";
+    const job = await this.connection();
+    const schema = this.options.connection.schema;
+
+    try {
+      metaData = await this.getIBMiMetaData(schema, job);
+      const query_object: Query<any> = await job.query<any>(query);
+      const result = await query_object.execute();
+      if (result.has_results && result.success) {
+        return new JSONToolOutput({ success: true, result: result.data });
+      } else {
+        return new JSONToolOutput({
+          success: false,
+          message: `No rows selected`,
+        });
+      }
+    } catch (error) {
+      const errorMessage = `Based on this database schema structure: ${metaData}, 
+      generate a correct query that retrieves data using the appropriate ibmi dialect. 
+      The original request was: ${query}, and the error was: ${error.message}.`;
+
+      throw new ToolError(errorMessage);
+    }
+  }
+
+  private async getIBMiMetaData(schema: string, job: mapepire.SQLJob): Promise<string> {
+    const sql = `
+      SELECT COLUMN_NAME, TABLE_NAME, DATA_TYPE
+      FROM QSYS2.SYSCOLUMNS
+      WHERE TABLE_SCHEMA = '${schema.toUpperCase()}'
+    `;
+
+    try {
+      const query = await job.query<any>(sql);
+      const metaData = await query.execute();
+      // console.log(metaData);
+      const tableMap = new Map<string, string[]>();
+
+      for (const item of metaData.data) {
+        const tableName = item["TABLE_NAME"];
+        const columnName = item["COLUMN_NAME"];
+        const dataType = item["DATA_TYPE"];
+        // console.log(tableName, columnName, dataType);
+        if (!tableMap.has(tableName)) {
+          tableMap.set(tableName, [
+            `Table '${tableName}' with columns: ${columnName} (${dataType})`,
+          ]);
+        } else {
+          tableMap.get(tableName)!.push(`${columnName} (${dataType})`);
+        }
+      }
+
+      return Array.from(tableMap.values())
+        .map((columns) => columns.join(", "))
+        .join("; ");
+    } catch (error) {
+      throw new ToolError(`Error initializing metadata: ${error}`, [], {
+        isRetryable: false,
+      });
+    }
+  }
+
+  private isReadOnlyQuery(query: string): boolean {
+    const normalizedQuery = query.trim().toUpperCase();
+    return (
+      normalizedQuery.startsWith("SELECT") ||
+      normalizedQuery.startsWith("SHOW") ||
+      normalizedQuery.startsWith("DESC")
+    );
+  }
+}
